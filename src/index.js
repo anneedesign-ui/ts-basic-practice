083c79d50fdb1843b930b6557e29de87a1277436
anneedesign-ui <anneedesign@gmail.com>
2025-11-27 19:24:06 +0900

    Add TypeScript basic practice

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// string (스트링)
+var userName = "Anne";
+// number (넘버)
+var age = 29;
+// boolean (불리언)
+var isStudent = true;
+// null (널)
+var nothingHere = null;
+// any (애니)
+var anything = "start";
+anything = 123;
+console.log("이름:", userName);
+console.log("나이:", age);
+console.log("학생인가요?:", isStudent);
+console.log("널 값:", nothingHere);
+console.log("any 타입 값:", anything);
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+// string (스트링)
+let userName: string = "Anne";
+
+// number (넘버)
+let age: number = 29;
+
+// boolean (불리언)
+let isStudent: boolean = true;
+
+// null (널)
+let nothingHere: null = null;
+
+// any (애니)
+let anything: any = "start";
+anything = 123;
+
+console.log("이름:", userName);
+console.log("나이:", age);
+console.log("학생인가요?:", isStudent);
+console.log("널 값:", nothingHere);
+console.log("any 타입 값:", anything);
